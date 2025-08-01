99eb23f028e9a41a5225af63022a500ef91fb697
raaajvett-stack <raaajvett@gmail.com>
2025-08-01 17:50:48 +0300

    Delete site directory

--- site/includes/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // إزالة كل متغيرات الجلسة
-session_destroy(); // إنهاء الجلسة
-header("site/auth/auth/login.php"); // إعادة التوجيه لصفحة تسجيل الدخول
-exit;
-?>
